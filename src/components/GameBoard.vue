417306fbbbd6fe191ce085f5ec478d0cd4bd6b58
Yehonatan <yehonatanbron@gmail.com>
2022-10-31 16:54:06 -0400

    quit button

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,36 +1,56 @@
 <template>
   <div>
-    <div>
-    <h1>TicTacToe</h1>
-    <p>rules: In order to win you must have 3 tokens either in a row, column, or diagonally</p>
+   <div v-if="quitButton">
+      <div>
+      <h1>TicTacToe</h1>
+      <p>Rules: In order to win you must have 3 tokens either in a row, column, or diagonally.</p>
+      </div>
+      <ul>
+        <div class="rows" id="row1">
+          <button class="buttons" id="button1"></button>
+          <button class="buttons" id="button2"></button>
+          <button class="buttons" id="button3"></button>
+        </div>
+        <div class="rows" id="row2">
+          <button class="buttons" id="button4"></button>
+          <button class="buttons" id="button5"></button>
+          <button class="buttons" id="button6"></button>
+        </div>
+        <div class="rows" id="row3">
+          <button class="buttons" id="button7"></button>
+          <button class="buttons" id="button8"></button>
+          <button class="buttons" id="button9"></button>
+        </div>
+      </ul>
+      <footer>
+        <button @click="quitButton = !quitButton">Quit Game</button>
+      </footer>
     </div>
-    <ul>
-      <div class="rows" id="row1">
-        <button class="buttons" id="button1"></button>
-        <button class="buttons" id="button2"></button>
-        <button class="buttons" id="button3"></button>
-      </div>
-      <div class="rows" id="row2">
-        <button class="buttons" id="button4"></button>
-        <button class="buttons" id="button5"></button>
-        <button class="buttons" id="button6"></button>
-      </div>
-      <div class="rows" id="row3">
-        <button class="buttons" id="button7"></button>
-        <button class="buttons" id="button8"></button>
-        <button class="buttons" id="button9"></button>
-      </div>
-    </ul>
+    <quitScreen v-else></quitScreen>
   </div>
 </template>
+
+<script>
+import quitScreen from "./quitScreen.vue";
+export default {
+  components: {
+    quitScreen
+  },
+  data() {
+    return {
+      quitButton: true
+    }
+  }
+}
+</script>
 
 <style>
     .buttons {
         padding: 100px;
     }
     div ul {
-      margin-left: 470px;
-      margin-top: 150px;
+      margin-left: 475px;
+      margin-top: 85px;
     }
     h1 {
       text-align: center;
@@ -38,6 +58,10 @@
     }
     p {
       font-size: 30px;
+      text-align: center;
+    }
+    footer {
+      margin-top: 50px;
       text-align: center;
     }
 </style>
---
+++ src/components/quitScreen.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <div>
+            <h1>ONLY LOSERS QUIT</h1>
+        </div>
+    </div>
+</template>
+
+<style>
+    h1 {
+        font-size: 500px;
+        text-align: center;
+    }
+</style>
