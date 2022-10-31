74b031ead6c64a2f771e7dd2fe2591ec34673193
Yehonatan <yehonatanbron@gmail.com>
2022-10-31 15:26:09 -0400

    created prompt and gameboard

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+
+
+<template>
+ <div>
+      <div v-if="startButton">
+        <button @click="startButton = !startButton">Start game</button>
+      </div>
+      <GameBoard v-else></GameBoard>
+  </div>
+
+</template>
+<script>
+import GameBoard from './components/GameBoard.vue';
+export default {
+  components: {
+    GameBoard
+  },
+  data() {
+    return {
+      startButton: true
+    }
+  }
+}
+</script>
+<style>
+
+</style>
---
+++ src/components/GameBoard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <ul>
+      <div class="rows" id="row1">
+        <button class="buttons" id="button1"></button>
+        <button class="buttons" id="button2"></button>
+        <button class="buttons" id="button3"></button>
+      </div>
+      <div class="rows" id="row2">
+        <button class="buttons" id="button4"></button>
+        <button class="buttons" id="button5"></button>
+        <button class="buttons" id="button6"></button>
+      </div>
+      <div class="rows" id="row3">
+        <button class="buttons" id="button7"></button>
+        <button class="buttons" id="button8"></button>
+        <button class="buttons" id="button9"></button>
+      </div>
+    </ul>
+  </div>
+</template>
+
+<style>
+    .buttons {
+        padding: 100px;
+        
+    }
+</style>
